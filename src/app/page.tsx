d4cbd5fc0bb02cd4125cf77692e19082472c4155
Krislv4nk <kristinalev4enko89@gmail.com>
2024-08-24 19:56:32 +0200

    Error Handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 
 import { headers } from 'next/headers';
 import AddCompanyButton from '@/app/components/add-company-button';
-import ClientComponent from '@/app/components/client-component';
-import ServerComponent from './components/server-component';
+
 
 export default function Home() {
   console.log(headers());
@@ -10,8 +9,7 @@ export default function Home() {
     <main >
       <h1 className='text-xl'>Home page {new Date().toTimeString()}</h1>
       <AddCompanyButton/>
-      <ClientComponent/>
-      <ServerComponent/>
+     
     </main>
   );
 }
