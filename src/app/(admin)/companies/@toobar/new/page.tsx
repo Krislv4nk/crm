6067945eced38b52bc39e275798fe281a3c6c48f
Krislv4nk <kristinalev4enko89@gmail.com>
2024-08-25 11:38:22 +0200

    Install react-query

--- src/app/(admin)/companies/@toobar/new/page.tsx
+++ src/app/(admin)/companies/@toobar/new/page.tsx
@@ -1,8 +1,16 @@
+'use client';
+
 import React from 'react';
-import Header from '@/app/components/header';
+import CompanyForm, { CompanyFieldValues } from '@/app/components/company-form';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
-  return null;
-}
+  return (
+    <div className="py-6 px-10">
+      <CompanyForm onSubmit={function (values: CompanyFieldValues): void | Promise<void> {
+        throw new Error('Function not implemented.');
+      } } />
+    </div>
+  );
+} 
