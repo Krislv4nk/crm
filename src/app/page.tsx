3307861ff5a5d28068c4510ef331e9fa02f5c8cc
Krislv4nk <kristinalev4enko89@gmail.com>
2024-08-25 12:10:02 +0200

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 
-import { headers } from 'next/headers';
-import AddCompanyButton from '@/app/components/add-company-button';
-
-
 export default function Home() {
-  console.log(headers());
   return (
-    <main >
-      <h1 className='text-xl'>Home page {new Date().toTimeString()}</h1>
-      <AddCompanyButton/>
-     
+    <main>
+      <h1 className="text-xl">Home page</h1>
     </main>
   );
 }
